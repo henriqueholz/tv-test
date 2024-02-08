a8117c01759fc6dba6b3f89fe3d51cedb735aed5
Henrique Castro <henriqueholz@live.com>
2024-02-07 21:12:27 -0300

    chore: add eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,12 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +18,15 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': [
+      'error',
+      {
+        'vars': 'all',
+        'args': 'after-used',
+        'ignoreRestSiblings': true,
+        'argsIgnorePattern': '^_'
+      }
+    ],
+    'react/react-in-jsx-scope': 'off'
   },
 }
